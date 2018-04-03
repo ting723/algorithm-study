1de9b8a4f485aab0e000e690166d105bf990ccdf
ting723 <ting723@gmail.com>
2018-04-03 17:47:53 +0800

    shellSort

--- src/main/java/zhanglw/study/algorithm/sort/ShellSort.java
+++ src/main/java/zhanglw/study/algorithm/sort/ShellSort.java
@@ -27,9 +27,58 @@ public class ShellSort {
         }
     }
 
+    /**
+     * 外部构建子序列
+     * 内部进行插入排序
+     */
+    public void execInnerInsertSort() {
+        int i, j, k, n = arr.length;
+
+        //构建步长
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (i = 0; i < gap; i++) {
+
+                // 以下为插入排序了
+                for (j = i; j < n; j += gap) {
+                    int key = arr[j];
+                    for (k = j - gap; k >= 0; k -= gap) {
+                        if (arr[k] > key) {
+                            arr[k + gap] = arr[k];
+                        } else {
+                            break;
+                        }
+                    }
+                    arr[k + gap] = key;
+                }
+            }
+        }
+    }
+
+    public void execOptimizeOne() {
+        int j, gap, n = arr.length;
+        for (gap = n / 2; gap > 0; gap /= 2) {
+            // 从第gap个元素开始
+            for (j = gap; j < n; j++) {
+                // 插入排序
+                if (arr[j] < arr[j - gap]) {
+                    // 小于-gap的,则依次向前移动
+                    int temp = arr[j];
+                    int k = j - gap;
+                    while (k >= 0 && arr[k] > temp) {
+                        arr[k + gap] = arr[k];
+                        k -= gap;
+                    }
+                    arr[k + gap] = temp;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         ShellSort shellSort = new ShellSort();
-        shellSort.exec();
+//        shellSort.exec();
+//        shellSort.execInnerInsertSort();
+        shellSort.execOptimizeOne();
         System.out.println(JSON.toJSONString(shellSort.arr));
     }
 }
